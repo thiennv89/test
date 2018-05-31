c0c94d8486a9f86a260a27b16bc70820a35607f4
thiennv89 <vietthien211@gmail.com>
2018-05-31 18:30:23 +0700

    first time test

---
+++ ex2.php
@@ -0,0 +1,11 @@
+<?php
+
+    function findUniqueDeliveryId($deliveryIds) {
+        $uId = 0;
+        foreach ($deliveryIds as $Id) {
+            $uId ^= $Id;
+        }
+        return $uId;
+    }
+    $deliveryIds = [1,2,3,4,5,5,4,2,1];
+    echo findUniqueDeliveryId($deliveryIds);
